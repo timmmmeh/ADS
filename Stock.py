b4a607ecf7b338d3b2633a1e74c66d8f8e8eeb12
timmmmeh <tim77377@gmail.com>
2015-09-16 14:52:41 +1200

    Stock exercise
    buys and sells stock

---
+++ Stock.py
@@ -0,0 +1,43 @@
+import heapq
+import random
+
+class BuySell:
+    def __init__(self):
+        self.buy = []
+        self.sell = []
+        self.gen_rand()
+
+    def gen_rand(self):
+        for i in range(10):
+            self.buy.append(random.randint(0, 100))
+            self.sell.append(random.randint(0, 100))
+        print("Random Numbers")
+        self.print_heap()
+        self.create_heap()
+
+
+    def create_heap(self):
+        heapq.heapify(self.buy)
+        heapq.heapify(self.sell)
+        print("Heapify")
+        self.print_heap()
+        self.work()
+
+    def print_heap(self):
+        print("buy: ", self.buy)
+        print("sell: ", self.sell)
+        print("")
+
+    def work(self):
+        print("Working")
+        for i in range(len(self.buy)):
+            self.print_heap()
+            if self.buy[0] < self.sell[0]:
+                heapq.heappop(self.buy)
+            else:
+                heapq.heappop(self.buy)
+                heapq.heappop(self.sell)
+
+
+if __name__ == '__main__':
+    BuySell()
