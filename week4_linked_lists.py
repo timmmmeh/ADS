f9ddf939d0c663c083e41c8fd37f8e7be7e026b9
timmmmeh <tim77377@gmail.com>
2015-10-22 23:35:25 +1300

    change
    something

---
+++ week4_linked_lists.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, cargo=None, next=None):
+        self.cargo = cargo
+        self.next  = next
+
+    def __str__(self):
+        return str(self.cargo)
+
+node = Node("test")
+print node
