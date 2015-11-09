514d51295cb489f6f6e952f4983a4bac0fcb14bc
timmmmeh <tim77377@gmail.com>
2015-11-09 13:07:32 +1300

    comment wordladder

--- Assignment 2/wordladder.py
+++ Assignment 2/wordladder.py
@@ -12,8 +12,8 @@ class Find:
         self.bad_words = []
         self.start_word = self.input()
         self.end_word = self.input()
-        """self.start_word = "three"
-        self.end_word = "honey"""""
+        # self.start_word = "issue"
+        # self.end_word = "weird"
         self.start_change()
 
     def start_change(self):
@@ -33,16 +33,30 @@ class Find:
             for l in string.ascii_lowercase:
                 word[letter] = l
                 new = ''.join(word)
-                #work on bad_words. not working yet
-                if self.search(new, self.dictionary) == True and new != unchanged and new != parents[len(parents)-1] and self.search(new, self.bad_words) == False:
+                # Checks if new is a real word, isn't the same word that was passed in, isnt the same word as the parent
+                # and isnt a word that has already been used.
+                if self.search(new, self.dictionary) == True and new != unchanged and new != parents[len(parents)-1] \
+                        and self.search(new, self.bad_words) == False:
+                    # creates a new list
                     lst = list(parents)
+                    # adds the new word to the new list
                     lst.append(new)
+                    # add the new word and the list to the queue
                     self.queue.put({new:lst})
+                    # add the new word to bad words so it wont be found again
                     self.bad_words.append(new)
+                    # sort bad words so that it can be searched through
                     self.bad_words.sort()
+                # if the new word is the end word then print the path
                 if new == self.end_word:
-                    print new, parents
+                    print_string = ""
+                    for i in range(len(parents)):
+                        print_string += parents[i] + " -> "
+                    print_string += new
+                    print print_string
                     return True
+                if len(self.dictionary) == len(self.bad_words):
+                    print "Connection can not be found"
             word = list(unchanged)
         return False
 
