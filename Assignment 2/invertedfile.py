8e1476fb0d567eed85d7372a0bd078c4a283a063
timmmmeh <tim77377@gmail.com>
2015-11-15 22:54:15 +1300

    add inverted file from assignment 2
    not final verrsion

---
+++ Assignment 2/invertedfile.py
@@ -0,0 +1,29 @@
+__author__ = 'Timmmmeh'
+class Invert:
+    def __init__(self):
+        self.dictionary_of_words = {"hello":[], "my":[], "name":[], "is":[], "tim":[], "welp":[]}
+        self.read_dictionary()
+        self.display()
+
+    def read_dictionary(self):
+        line_count = 0
+        with open("Document") as item:
+            for line in item.readlines():
+                word_count = 0
+                line_count += 1
+                words = line.lower()[:-1]
+                for x in words.split(" "):
+                    word_count += 1
+                    for i in range(len(self.dictionary_of_words)):
+                        if x == self.dictionary_of_words.items()[i][0]:
+                            self.dictionary_of_words.items()[i][1].append([line_count, word_count])
+
+    def display(self):
+        for i in range(len(self.dictionary_of_words)):
+            string = self.dictionary_of_words.items()[i][0]
+            for x in range(len(self.dictionary_of_words.items()[i][1])):
+                string += " " + str(self.dictionary_of_words.items()[i][1][x])
+            print string
+
+if __name__ == '__main__':
+    Invert()
