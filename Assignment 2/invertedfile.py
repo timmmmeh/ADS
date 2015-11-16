7a7548d0eb6a89f14bc79bec5cede7b109645d39
timmmmeh <tim77377@gmail.com>
2015-11-16 14:42:52 +1300

    made more robust
    check for punctuation and stuff

--- Assignment 2/invertedfile.py
+++ Assignment 2/invertedfile.py
@@ -1,8 +1,13 @@
-__author__ = 'Timmmmeh'
+__author__ = 'jonet5'
+import collections
+
 class Invert:
     def __init__(self):
-        self.dictionary_of_words = {"hello":[], "my":[], "name":[], "is":[], "tim":[], "welp":[]}
+        self.puncList = [".",";",":","!","?","/","\\",",","#","@","$","&",")","(","\""]
+        self.dictionary_of_words = {}
+        self.words_used = []
         self.read_document()
+        self.dictionary_of_words = collections.OrderedDict(sorted(self.dictionary_of_words.items()))
         self.display()
 
     def read_document(self):
@@ -13,10 +18,17 @@ class Invert:
                 line_count += 1
                 words = line.lower()[:-1]
                 for x in words.split(" "):
+                    for i in range(len(self.puncList)):
+                        x = x.replace(self.puncList[i], "")
+                    x = x.lower()
                     word_count += 1
-                    for i in range(len(self.dictionary_of_words)):
-                        if x == self.dictionary_of_words.items()[i][0]:
-                            self.dictionary_of_words.items()[i][1].append([line_count, word_count])
+                    if x not in self.words_used:
+                        self.words_used.append(x)
+                        self.dictionary_of_words.update({x:[[line_count,word_count]]})
+                    else:
+                        for i in range(len(self.dictionary_of_words)):
+                            if x == self.dictionary_of_words.items()[i][0]:
+                                self.dictionary_of_words.items()[i][1].append([line_count, word_count])
 
     def display(self):
         for i in range(len(self.dictionary_of_words)):
