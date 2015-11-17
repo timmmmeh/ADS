338efade2fce13b6dc207d574fd309224157d0d5
timmmmeh <tim77377@gmail.com>
2015-11-17 13:55:27 +1300

    added comments

--- Assignment 2/PostfixCalculator.py
+++ Assignment 2/PostfixCalculator.py
@@ -1,6 +1,7 @@
 import operator
-
 __author__ = 'Timmmmeh'
+
+#stack from the internet
 class Stack:
     def __init__(self):
         self.items = []
@@ -15,15 +16,20 @@ class Stack:
     def size(self):
         return len(self.items)
 s = Stack()
+#if any operators are called run the right method
 operators = {"+": operator.add, "-":operator.sub, "*":operator.mul, "/":operator.div}
-print("Enter the Polish notation equation you want solved in this notation: 76 43 +")
+print("Enter the Polish notation equation you want solved")
 equation = raw_input("")
+#split the equation into the right parts
 equation = equation.split(" ")
 for i in range(len(equation)):
     if equation[i] in operators:
+        #rearange the values that come of first so it does devision/ subtracting right
         two = s.pop()
         one = s.pop()
+        #do the equation
         new_value = operators[equation[i]](one, two)
+        #push the new value onto the stack
         s.push(new_value)
     else:
         s.push(int(equation[i]))
--- Assignment 2/invertedfile.py
+++ Assignment 2/invertedfile.py
@@ -3,25 +3,37 @@ import collections
 
 class Invert:
     def __init__(self):
+        #list of possible punctuation
         self.puncList = [".",";",":","!","?","/","\\",",","#","@","$","&",")","(","\""]
+        #create the dict of words
         self.dictionary_of_words = {}
+        #create list of words that have already been added to the dict
         self.words_used = []
+        #reads the Document file
         self.read_document()
+        #sorts the dictionary
         self.dictionary_of_words = collections.OrderedDict(sorted(self.dictionary_of_words.items()))
+        #prints the dictionary
         self.display()
 
     def read_document(self):
+        #create the line coordinate
         line_count = 0
         with open("Document") as item:
             for line in item.readlines():
+                #creates the word coordinate
                 word_count = 0
                 line_count += 1
+                #if at the end remove the /n
                 words = line.lower()[:-1]
                 for x in words.split(" "):
                     for i in range(len(self.puncList)):
+                        #replace all punctuation with nothing
                         x = x.replace(self.puncList[i], "")
+                    #convert all uppercase letters to lowercase
                     x = x.lower()
                     word_count += 1
+                    #check if the word has already been used
                     if x not in self.words_used:
                         self.words_used.append(x)
                         self.dictionary_of_words.update({x:[[line_count,word_count]]})
